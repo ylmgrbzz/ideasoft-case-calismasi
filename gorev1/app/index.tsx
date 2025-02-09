76ac73a84769f6269f341fe57d04ebf880bbef0f
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2025-02-09 18:01:31 +0300

    Refactor app structure and remove tab navigation

---
+++ gorev1/app/index.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+export default function HomeScreen() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText style={styles.text}>ylmgrbz</ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
