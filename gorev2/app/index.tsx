b872012ae4eb8a8595826336caccb9cc433113a1
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2025-02-09 20:35:14 +0300

    I apolog

---
+++ gorev2/app/index.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+export default function HomeScreen() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">Yönetim Paneli</ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
